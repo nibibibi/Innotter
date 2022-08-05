5bc7f0831a2c21387aa3f3e9feddd615ef306d5a
Dmitry Shaplyko <shaplykodmitry@gmail.com>
2022-08-05 13:25:22 +0300

    Removed spaces between fields

--- Innotter/pages/models.py
+++ Innotter/pages/models.py
@@ -10,27 +10,21 @@ class Page(models.Model):
     uuid = models.CharField(max_length=30, unique=True)
     description = models.TextField()
     tags = models.ManyToManyField("pages.Tag", related_name="pages")
-
     owner = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="pages"
     )
     followers = models.ManyToManyField("users.User", related_name="follows")
-
     image = models.URLField(null=True, blank=True)
-
     is_private = models.BooleanField(default=False)
     follow_requests = models.ManyToManyField("users.User", related_name="requests")
-
     unblock_date = models.DateTimeField(null=True, blank=True)
 
 
 class Post(models.Model):
     page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="posts")
     content = models.CharField(max_length=180)
-
     reply_to = models.ForeignKey(
         "pages.Post", on_delete=models.SET_NULL, null=True, related_name="replies"
     )
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- Innotter/users/models.py
+++ Innotter/users/models.py
@@ -11,6 +11,5 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     image_s3_path = models.CharField(max_length=200, null=True, blank=True)
     role = models.CharField(max_length=9, choices=Roles.choices)
-
     title = models.CharField(max_length=80)
     is_blocked = models.BooleanField(default=False)
