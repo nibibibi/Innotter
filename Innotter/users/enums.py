958d0f0b45b51965c79ccf28b2f0415d9ed121ae
Dmitry Shaplyko <shaplykodmitry@gmail.com>
2022-08-05 12:42:07 +0300

    Fixed commented issues

---
+++ Innotter/users/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class RolesEnum(Enum):
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
--- Innotter/users/models.py
+++ Innotter/users/models.py
@@ -1,17 +1,14 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from enums import RolesEnum
 
 
 class User(AbstractUser):
-   class Roles(models.TextChoices):
-       USER = 'user'
-       MODERATOR = 'moderator'
-       ADMIN = 'admin'
+    roles = RolesEnum()
 
-   email = models.EmailField(unique=True)
-   image_s3_path = models.CharField(max_length=200, null=True, blank=True)
-   role = models.CharField(max_length=9, choices=Roles.choices)
+    email = models.EmailField(unique=True)
+    image_s3_path = models.CharField(max_length=200, null=True, blank=True)
+    role = models.CharField(max_length=9, choices=Roles.choices)
 
-   title = models.CharField(max_length=80)
-   is_blocked = models.BooleanField(default=False)
-
+    title = models.CharField(max_length=80)
+    is_blocked = models.BooleanField(default=False)
