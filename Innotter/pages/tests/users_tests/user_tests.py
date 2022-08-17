254c11763fd951a606cea5f06a7c7f56226e9921
Dmitry Shaplyko <shaplykodmitry@gmail.com>
2022-08-17 15:28:11 +0300

    List favourites unit test

--- Innotter/pages/tests/users_tests/user_tests.py
+++ Innotter/pages/tests/users_tests/user_tests.py
@@ -7,6 +7,7 @@ from pages.views.user_views import UserViewSet
 
 pytestmark = pytest.mark.django_db
 toggle_block_unblock_view = UserViewSet.as_view({'post': "toggle_block"})
+list_favourite_posts_view = UserViewSet.as_view({'get': "list_favourites"})
 
 
 class TestUserLogic:
@@ -25,3 +26,13 @@ class TestUserLogic:
         assert response.data.get('status') == "user blocked"
         response = toggle_block_unblock_view(request, pk=user_to_block.pk)
         assert response.data.get('status') == "user unblocked"
+
+    @mock.patch("Innotter.settings.SECRET_KEY", "1")
+    def test_list_favourites(self, user: user, api_factory: APIRequestFactory):
+        token = generate_access_token(user)
+        request = api_factory.get(f"{self.url}/{user.pk}/list_favourites")
+        force_authenticate(request, user=user, token=token)
+        response = list_favourite_posts_view(request)
+
+        assert response.status_code == 200
+        # TODO: maybe add a post to favourites and check if it will be in json response
