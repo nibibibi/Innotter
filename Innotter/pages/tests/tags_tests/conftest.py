935e09c174763830c4eb020d1b9c826b86dd9d4e
Dmitry Shaplyko <shaplykodmitry@gmail.com>
2022-08-19 13:56:26 +0300

    Tag unit tests competed

--- Innotter/pages/tests/tags_tests/conftest.py
+++ Innotter/pages/tests/tags_tests/conftest.py
@@ -2,6 +2,12 @@ import pytest
 from model_bakery import baker
 from pages.models import Tag
 
+
 @pytest.fixture
 def tag():
     return baker.make(Tag)
+
+
+@pytest.fixture
+def tags():
+    return baker.make(Tag, _quantity=150)
--- Innotter/pages/tests/tags_tests/tag_tests.py
+++ Innotter/pages/tests/tags_tests/tag_tests.py
@@ -3,6 +3,7 @@ from unittest import mock
 from pages.tests.users_tests.conftest import user
 from pages.models import Tag
 from rest_framework.test import APIRequestFactory, force_authenticate
+from pages.tests.tags_tests.conftest import tag, tags
 from pages.views.tag_views import TagViewSet
 from users.auth import generate_access_token
 from pages.serializers.tag_serializers import TagSerializer
@@ -26,3 +27,32 @@ class TestTagLogic:
         response = default_viewset(request)
 
         assert response.status_code == 201 and response.data == TagSerializer(Tag.objects.first()).data
+
+    @mock.patch("Innotter.settings.SECRET_KEY", "1")
+    def test_destroy_tag(self, user: user, tag: tag, api_factory: APIRequestFactory):
+        assert len(Tag.objects.all()) == 1 and Tag.objects.first() == tag
+        request = api_factory.delete(f"{self.url}{tag.pk}")
+        token = generate_access_token(user)
+        force_authenticate(request=request, user=user, token=token)
+        response = default_viewset(request, pk=tag.pk)
+
+        assert response.status_code == 204 and len(Tag.objects.all()) == 0
+
+    @mock.patch("Innotter.settings.SECRET_KEY", "1")
+    def test_retrieve_tag(self, user: user, tag: tag, api_factory: APIRequestFactory):
+        request = api_factory.get(f"{self.url}{tag.pk}")
+        token = generate_access_token(user)
+        force_authenticate(request=request, user=user, token=token)
+        response = default_viewset(request, pk=tag.pk)
+
+        assert response.status_code == 200 and response.data == TagSerializer(tag).data
+
+    @mock.patch("Innotter.settings.SECRET_KEY", "1")
+    def test_list_tags(self, user: user, tags: tags, api_factory: APIRequestFactory):
+        request = api_factory.get(f"{self.url}")
+        token = generate_access_token(user)
+        force_authenticate(request=request, user=user, token=token)
+        response = list_viewset(request)
+
+        assert response.status_code == 200 and len(response.data) == len(tags)
+        assert response.data == TagSerializer(tags, many=True).data
